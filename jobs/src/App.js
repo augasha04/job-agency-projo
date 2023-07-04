8777b2421a66a10dbeeca43589b0636b8e9d5fbf
augasha04 <augustinemwangi04@gmail.com>
2023-07-04 11:27:23 +0300

    create a navbar function and done the imports

--- jobs/src/App.js
+++ jobs/src/App.js
@@ -1,12 +1,12 @@
-
-import './App.css';
+import React from 'react'
+import NavBar from './jobseekercomponents/NavBar'
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <NavBar/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
